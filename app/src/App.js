d76bb64f17d4261eb91bb699f6c97b6764072316
Huy23232 <98579579+DevHuy11@users.noreply.github.com>
2022-01-28 22:27:50 +0700

    .

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import Home from './components/body/home/Home';
 
 function App() {
+  const [data, setdata] = useState({})
+
+  useEffect(() => {
+    fetch("./data.json").then((res) => {
+      return res.json()
+    })
+    .then(res => {
+      setdata(res)
+      console.log(data)
+    })
+    .catch((err)=>console.log(err))
+  })
+
   return (
     <div className="App">
         <Home />
